cedf53f7b74eeaf0dd84f49f74a7b199612217c3
Michał Basiński <michal.basinski88@gmail.com>
2019-09-28 11:37:08 +0200

    writer

--- src/main/java/pl/sda/files/simple/App.java
+++ src/main/java/pl/sda/files/simple/App.java
@@ -4,5 +4,8 @@ class App {
     public static void main(String args[]) {
         SimpleFileReader reader = new SimpleFileReader();
         reader.readFile("/home/michal/SDA/Java19/Projekty/SDA_J19_Files/src/main/resources/simple.txt");
+
+        SimpleFileWriter writer = new SimpleFileWriter();
+        writer.write("Moja zawartość", "/home/michal/tttt");
     }
 }
