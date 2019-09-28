bcf2e7b24c6bbdb98d1d9c993b058178d5c17216
Michał Basiński <michal.basinski88@gmail.com>
2019-09-28 15:55:10 +0200

    JSON readers

---
+++ src/main/java/pl/sda/files/json/App.java
@@ -0,0 +1,16 @@
+package pl.sda.files.json;
+
+import java.io.IOException;
+
+class App {
+    public static void main(String[] args) throws IOException {
+        JsonReader reader = new JsonReader();
+        Person person = reader.read("/home/michal/SDA/Java19/Projekty/SDA_J19_Files/src/main/resources/person.json");
+
+        JsonAltReader altReader = new JsonAltReader();
+        Person person2 = altReader.read("/home/michal/SDA/Java19/Projekty/SDA_J19_Files/src/main/resources/person.json");
+
+
+        int i = 0;
+    }
+}
