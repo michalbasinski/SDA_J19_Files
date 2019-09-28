1c3323176f83651ed0dc54c766ea96033f53485b
Michał Basiński <michal.basinski88@gmail.com>
2019-09-28 13:55:24 +0200

    CSV Writer

--- src/main/java/pl/sda/files/csv/App.java
+++ src/main/java/pl/sda/files/csv/App.java
@@ -7,6 +7,8 @@ class App {
     public static void main(String[] args) {
         CSVReader csvReader = new CSVReader();
         List<Person> personList = csvReader.read("/home/michal/SDA/Java19/Projekty/SDA_J19_Files/src/main/resources/persons.csv");
-        int i = 0;
+
+        CSVWriter writer = new CSVWriter();
+        writer.write(personList, "/home/michal/out.csv");
     }
 }
---
+++ src/main/java/pl/sda/files/csv/CSVWriter.java
@@ -0,0 +1,33 @@
+package pl.sda.files.csv;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+class CSVWriter {
+    void write(List<Person> persons, String filePath) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath))) {
+
+            String headerRow = "name;lastName;age;sex;salary";
+            bufferedWriter.write(headerRow);
+            bufferedWriter.newLine();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Person person : persons) {
+                stringBuilder.setLength(0);
+                stringBuilder.append(person.getName()).append(";")
+                        .append(person.getLastName()).append(";")
+                        .append(person.getAge()).append(";")
+                        .append(person.getSex().toString()).append(";")
+                        .append(person.getSalary());
+                bufferedWriter.write(stringBuilder.toString());
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
