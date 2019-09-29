f0705b01c5c1ad56d2e785ca38074c7aa0173c4e
Michał Basiński <michal.basinski88@gmail.com>
2019-09-29 10:00:53 +0200

    JSON writer

--- src/main/java/pl/sda/files/json/App.java
+++ src/main/java/pl/sda/files/json/App.java
@@ -10,6 +10,8 @@ class App {
         JsonAltReader altReader = new JsonAltReader();
         Person person2 = altReader.read("/home/michal/SDA/Java19/Projekty/SDA_J19_Files/src/main/resources/person.json");
 
+        JSONWriter writer = new JSONWriter();
+        writer.write(person2, "/home/michal/personFromWriter.json");
 
         int i = 0;
     }
